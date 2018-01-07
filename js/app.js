42f9fa42830d7a7fcf7451c33d8202b0801a140a
wendytamara <anacarlavegam@gmail.com>
2018-01-07 06:22:37 -0500

    añadiendo extructura-archivos base

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDKNjzBbhY9GG9XAwbjig7_zk6DnjySntw",
+  authDomain: "red-social-mayoristas.firebaseapp.com",
+  databaseURL: "https://red-social-mayoristas.firebaseio.com",
+  projectId: "red-social-mayoristas",
+  storageBucket: "",
+  messagingSenderId: "961495248539"
+};
+firebase.initializeApp(config);
+var holaMundo = document.getElementById('holaMundo')
+var dbRef = firebase.database().ref().child('text');
+dbRef.on('value', snap => holaMundo.innerText = snap.val() );
