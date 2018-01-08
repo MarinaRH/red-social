4bb95547302559785700db4c1000894a11724e21
wendytamara <anacarlavegam@gmail.com>
2018-01-07 23:07:12 -0500

    crear usuario e iniciar sesion con firebase

--- js/app.js
+++ js/app.js
@@ -15,4 +15,74 @@ dbRef.on('value', snap => holaMundo.innerText = snap.val() );
 // inicializar formulario materialize
 $(document).ready(function() {
   $('select').material_select();
+
+  // variables crear nuevo usuario
+  var $btnCreate = $('#btnCreate');
+  var $emailCreate = $('#emailCreate');
+  var $passwordCreate = $('#passwordCreate');
+  // variables iniciar sesion
+  var $btnLogIn = $('#btnLogIn');
+  var $email = $('#email');
+  var $password = $('#password');
+
+
+  $btnCreate.on('click', createNewUsers);
+  $btnLogIn.on('click', logIn);
+
+  // crear nuevo usuario
+   function createNewUsers() {
+     firebase.auth().createUserWithEmailAndPassword($emailCreate.val(), $passwordCreate.val()).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+      console.log(errorCode);
+      console.log(errorMessage);
+    });
+  }
+    // iniciar sesion
+  function logIn() {
+    firebase.auth().signInWithEmailAndPassword($email.val(), $password.val()).catch(function(error) {
+        // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+        // ...
+        console.log(errorCode);
+        console.log(errorMessage);
+      });
+    }
+
+  function observer() {
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        // User is signed in.
+        console.log('el usuario a iniciado sesion');
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+        var providerData = user.providerData;
+        // ...
+      } else {
+
+        console.log('el usuario esta desconectado');
+      }
+    });
+  }
+  observer();
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
