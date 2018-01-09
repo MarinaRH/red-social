5bf08d4a89c4f7dfcba3dca7410a3ad0703a1138
MarinaRH <l_marina1717@hotmail.com>
2018-01-09 02:12:27 -0500

    merge manual

--- js/app.js
+++ js/app.js
@@ -9,43 +9,7 @@ var config = {
 };
 
 firebase.initializeApp(config);
-<<<<<<< HEAD
 
-
-var holaMundo = document.getElementById('holaMundo')
-var dbRef = firebase.database().ref().child('text');
-dbRef.on('value', snap => holaMundo.innerText = snap.val() );
-=======
-var ShowComment = function(e){
-  $('#btn-text').on('click',function(e){
-    var texto = $('#new-text').val();
-    $('#publicacion').append('<div id="public-header" class="col s12 m12"><div class="col s2 m2"><img src="../assets/images/perfil1.jpg" alt="" class="circle img-perfil"></div><div class="col s10 m10">Mario Belmont</div><div class="col s12 m12 divider"></div></div><div id="public-body" class="col s12 m12"><div class="text-public"><p>'+ texto +'</p></div><div class=" col s12 m12 divider"></div></div><div class="col s12 m12"><a><i class="fa fa-thumbs-o-up icon-public"></i></a><a href="#" id="icon-comment"><i class="fa fa-edit icon-public"></i></a><a><i class="fa fa-share icon-public"></i></a><div class="col s12 m12"><input id="input" placeholder="Add a comment.." type="text"></div></div>');
-
-    // objDb.publicacion.push({
-    //    		text: texto
-    //    	})
-    //    guardarDatos(objDb);
-  })
-}
-ShowComment();
-
-$('a#icon-comment').on('click',function(){
-  $('input').addClass('blue');
-});
-
-var guardarDatos =(publicacion)=>{
- 	database.ref("/").set(publicacion);
- }
-var database = firebase.database();
-// Leer datos: Usar el método .on('value')
-database.ref("/publicacion").on('value', (snapshot)=>{
-	let publicacion = snapshot.val();
-	objDb.publicacion = publicacion;
-	ShowComment(publicacion);
-});
-});
-
->>>>>>> 7664ebcd406e8481a0758b557fa5909239d5f2ab
 
 // inicializar formulario materialize
 $(document).ready(function() {
