eb1e7ef1d5b987241c796e54ad4ea448c7bbcf46
wendytamara <anacarlavegam@gmail.com>
2018-01-10 08:54:20 -0500

    añadiendo js

--- js/Data.js
+++ js/Data.js
@@ -1,50 +1,79 @@
-var data = [
-  {
-    'name':'DeModa',
-    'email':'demoda@gmail.com',
-    'address':'San Borja Este n° 876',
-    'phone':'(01) 374-9777',
-    'descripcion':'Ofrecemos excelentes precios por mayor y un servicio personalizado sin     sacrificar nuestros diseños y la calidad de nuestras prendas femeninas.',
-    'img':'../assets/images/dmoda.png'
-  },
-  {
-    'name':'ETNIKA',
-    'email':'etnika@gmail.com',
-    'address':'Av. dos de mayo n° 793, San Isidro',
-    'phone':'(054) 999786543',
-    'descripcion':'ETNIKA nace de la interrelación entre un producto de diseño propio, de     calidad y con una imagen de marca coherente y unificada.',
-    'img':'../assets/images/etnika.png'
-  },
-  {
-    'name':'Zaris',
-    'email':'zaris@gmail.com',
-    'address':'Av. JavierPrado este n°66',
-    'phone':'(01)374777-989876766',
-    'descripcion':'Prendas hechas con el mejor ALGODÓN de calidad y buen precio para los      pequeños del hogar.',
-    'img':'../assets/images/zaris.jpg'
-  },
-  {
-    'name':'Ana Mendoza',
-    'email':'ana@gmail.com',
-    'profession':'Software development',
-    'city':'Lima-Peru',
-    'born':' August 12, 1991',
-    'img':'../assets/images/perfil1.jpg'
-  },
-  {
-    'name':'Rossy Rodriguez Guevara',
-    'email':'rossy@gmail.com',
-    'profession':'FrontEnd development',
-    'city':'Arequipa-Peru',
-    'born':' April 12, 1990',
-    'img':'../assets/images/perfil2.jpg'
-  },
-  {
-    'name':'Carla Nuñez Pinto',
-    'email':'carla@gmail.com',
-    'profession':'Ux development',
-    'city':'Cusco-Peru',
-    'born':' March 15, 1989',
-    'img':'../assets/images/perfil3.jpg'
+var data = {
+  'anacarlavegam@gmail.com': {
+    'friends': [
+     {
+      'name': 'Lorena Mendoza',
+      'photo': 'https://cdn.productplan.com/wp-content/uploads/2016/06/Maddy-Kirsch-300x300.jpg',
+      'address':'San Borja Este n° 876',
+      'phone':'(01) 374-9777',
+      'descripcion':'Ofrecemos excelentes precios por mayor y un servicio     personalizado sin     sacrificar nuestros diseños y la calidad de nuestras prendas femeninas.'
+      },
+      {
+        'name': 'Leslie Martinez',
+        'photo': 'http://www.grandespymes.com.ar/wp-content/uploads/2017/06/persona_irresistible.jpg',
+        'address':'San Borja Este n° 876',
+        'phone':'(01) 374-9777',
+        'descripcion':'Ofrecemos excelentes precios por mayor y un servicio     personalizado sin     sacrificar nuestros diseños y la calidad de nuestras prendas femeninas.'
+      },
+      {
+        'name': 'Leslie Martinez',
+        'photo': 'https://www.provincial.com/fbin/mult/Cuenta_corriente_Persona_Jur_dica_150_tcm1305-467437.png',
+        'address':'San Borja Este n° 876',
+        'phone':'(01) 374-9777',
+        'descripcion':'Ofrecemos excelentes precios por mayor y un servicio     personalizado sin     sacrificar nuestros diseños y la calidad de nuestras prendas femeninas.'
+      }
+    ]
   }
-]
+}
+
+
+ // [
+//   {
+//     'name':'Laura Rodriguez',
+//     'email':'demoda@gmail.com',
+//     'address':'San Borja Este n° 876',
+//     'phone':'(01) 374-9777',
+//     'descripcion':'Ofrecemos excelentes precios por mayor y un servicio personalizado sin     sacrificar nuestros diseños y la calidad de nuestras prendas femeninas.',
+//     'img':'../assets/images/dmoda.png'
+//   },
+//   {
+//     'name':'ETNIKA',
+//     'email':'etnika@gmail.com',
+//     'address':'Av. dos de mayo n° 793, San Isidro',
+//     'phone':'(054) 999786543',
+//     'descripcion':'ETNIKA nace de la interrelación entre un producto de diseño propio, de     calidad y con una imagen de marca coherente y unificada.',
+//     'img':'../assets/images/etnika.png'
+//   },
+//   {
+//     'name':'Zaris',
+//     'email':'zaris@gmail.com',
+//     'address':'Av. JavierPrado este n°66',
+//     'phone':'(01)374777-989876766',
+//     'descripcion':'Prendas hechas con el mejor ALGODÓN de calidad y buen precio para los      pequeños del hogar.',
+//     'img':'../assets/images/zaris.jpg'
+//   },
+//   {
+//     'name':'Ana Mendoza',
+//     'email':'anamendoza@gmail.com',
+//     'profession':'Software development',
+//     'city':'Lima-Peru',
+//     'born':' August 12, 1991',
+//     'img':'../assets/images/perfil1.jpg'
+//   },
+//   {
+//     'name':'Rossy Rodriguez Guevara',
+//     'email':'rossyrodriguez@gmail.com',
+//     'profession':'FrontEnd development',
+//     'city':'Arequipa-Peru',
+//     'born':' April 12, 1990',
+//     'img':'../assets/images/perfil2.jpg'
+//   },
+//   {
+//     'name':'Carla Nuñez Pinto',
+//     'email':'carlapinto@gmail.com',
+//     'profession':'Ux development',
+//     'city':'Cusco-Peru',
+//     'born':' March 15, 1989',
+//     'img':'../assets/images/perfil3.jpg'
+//   }
+// ]
