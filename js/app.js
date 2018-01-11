6b68f7ff77eabe2a3a63b49ec8eb44dbe268989a
mariaahv <mariahv14@hotmail.com>
2018-01-11 10:41:13 -0500

    corrigiendo errores de identacion

--- js/app.js
+++ js/app.js
@@ -134,6 +134,7 @@ $(document).ready(function() {
         var $nameUsers = $('#nameUsers');
         var $coments = $('#coments');
         var $usersComent = $('#usersComent');
+        var $coments1 = $('#coments1');
         var $dataPhoto = data["anacarlavegam@gmail.com"]["friends"];
 
 
@@ -156,6 +157,7 @@ $(document).ready(function() {
 
         $photoProfile.attr('src', photoURL);
         $coments.attr('src', photoURL);
+        $coments1.attr('src', photoURL);
         $nameUsers.text(displayName);
         $usersComent.text(displayName);
 
@@ -189,17 +191,19 @@ $(document).ready(function() {
 
   function logInGoogle() {
     event.preventDefault();
-    window.location.href = '../views/home.html'
+
 
     var provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider).then(function(result) {
       user = result.user;
       console.log(user);
+
+      observer();
       initApp();
+
+      window.location.href = '../views/home.html';
     });
   }
-
- // ***************************validando login con users de Data
 
 
   function initApp() {
@@ -242,14 +246,14 @@ $(document).ready(function() {
       var texto = $newPost.val();
       $newPost.val('');
       observer();
-      $('#publicacion').append('<div id="public-header" class="col s12 m12 white"><div class="col s2 m2 white"><img  id="coments" alt="" class="img-perfil"></div><div id="usersComent" class="col s10 m10 white"><br><span class="grey-text">Publicado a las :'+getTime()+'</span><br></div><div class="col s12 m12 divider"></div></div><div id="public-body" class="col s12 m12 white"><div class="text-public"><p>'+ texto +'</p></div></div><div class="col s12 m12 white"><a><i class="fa fa-thumbs-o-up icon-public" id="icon-like"></i></a><a href="#"><i class="fa fa-edit icon-public"></i></a><a><i class="fa fa-share icon-public"></i></a><p class="right grey-text" id="number-likes"> likes</p><div class="col s12 m12 divider"></div><br><br><div id="add-comment" class="col s12 m12"></div></div>');
+      $('#publicacion').append('<div id="public-header" class="col s12 m12 white"><div class="col s2 m2 white"><img  id="coments1" alt="" class="img-perfil"></div><div id="usersComent" class="col s10 m10 white"><br><span class="grey-text">Publicado a las :'+getTime()+'</span><br></div><div class="col s12 m12 divider"></div></div><div id="public-body" class="col s12 m12 white"><div class="text-public"><p>'+ texto +'</p></div></div><div class="col s12 m12 white"><a><i class="fa fa-thumbs-o-up icon-public" id="icon-like"></i></a><a href="#"><i class="fa fa-edit icon-public"></i></a><a><i class="fa fa-share icon-public"></i></a><p class="right grey-text" id="number-likes"> likes</p><div class="col s12 m12 divider"></div><br><br><div id="add-comment" class="col s12 m12"></div></div>');
 
       $('#input-comment').removeClass('hide');
       $btnPost.attr('disabled', true);
       $btnPost.removeClass('btn-grad');
     })
   }
-  
+
   ShowPublic();
 
   // Función para agregar hora
