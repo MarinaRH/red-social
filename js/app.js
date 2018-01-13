d472a0275f96b60f751d51cb58fa1a6cd9d55c37
MarinaRH <l_marina1717@hotmail.com>
2018-01-13 01:13:09 -0500

    modificando clases

--- js/app.js
+++ js/app.js
@@ -222,7 +222,7 @@ $(document).ready(function() {
   $btnPost.on('click', function(e) {
     var texto = $newPost.val();
     $newPost.val('');
-    $('#publicacion').prepend('<div class="col s12 m12 border-post"><div id="public-header" class="col s12 m12"><br><div class="col s2 m2"><img id="photoProfile" class=" img-perfil "></div><div class="col s10 m10  usersComent"><br><span class="grey-text">Publicado a las :'+getTime()+'</span><br></div><div class="col s12 m12 divider"></div></div><div id="public-body" class="col s12 m12 "><div class="text-public"><p>'+ texto +'</p></div></div><div class="col s12 m12 divider"></div><div class="col s12 m12 "><a><i class="fa fa-thumbs-o-up icon-public" id="icon-like"></i></a><a href="#"><i class="fa fa-edit icon-public"></i></a><a><i class="fa fa-share icon-public"></i></a><p class="right grey-text" id="contador"></p><div id="add-comment" class="col s12 m12"></div></div><div class="col s12 m12 "><input id="input-comment" placeholder="Add a comment.." type="text"></div></div>');
+    $('#publicacion').prepend('<div class="col s12 m12 border-post"><div id="public-header" class="col s12 m12"><br><div class="col s2 m2"><img id="photoProfile" class=" img-perfil "></div><div class="col s10 m10  usersComent"><br><span class="grey-text">Publicado a las :'+getTime()+'</span><br></div><div class="col s12 m12 divider"></div></div><div id="public-body" class="col s12 m12 "><div class="text-public"><p>'+ texto +'</p></div></div><div class="col s12 m12 divider"></div><div class="col s12 m12 "><a><i class="fa fa-thumbs-o-up icon-public" id="icon-like"></i></a><a href="#"><i class="fa fa-edit icon-public"></i></a><a><i class="fa fa-share icon-public"></i></a><p class="right grey-text" id="contador"></p><div id="add-comment" class="col s12 m12"></div></div><div class="col s12 m12 "><input id="input-comment" placeholder="Add a comment.." type="text"></div><br></div>');
 
     $btnPost.attr('disabled', true);
     $btnPost.removeClass('btn-grad');
@@ -265,7 +265,7 @@ $(document).ready(function() {
       var comentario = $('#input-comment').val();
       $('#input-comment').val('');
       if(comentario){
-        $('#add-comment').append('<div class="col s1 m1"><img id="photoProfile" class=" img-perfil" alt="" ></div><div class="col s10 m10 white usersComent"></div><p class="col s11 m11 ">'+comentario+'<span  class="right grey-text">publicado : '+getTime()+'</span></p>');
+        $('#add-comment').append('<div class="col s1 m1"><img id="photoProfile" class=" img-perfil" alt="" ></div><div class="col s10 m10 white usersComent"></div><p class="col s11 m11 ">'+comentario+'<span  class="right grey-text">publicado : '+getTime()+'</span></p><');
       }else{
         $('#input-comment').val('');
       }
