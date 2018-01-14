fb58d7cfbf41003c97ce7c7762dbc21df3a2bbf2
wendytamara <anacarlavegam@gmail.com>
2018-01-14 10:52:24 -0500

    corrigiendo error al subir imagenes

--- js/app.js
+++ js/app.js
@@ -256,14 +256,23 @@ $(document).ready(function() {
   $('input[type=file]').change(function() {
     var file = (this.files[0].name).toString();
     var reader = new FileReader();
+    debugger
 
   reader.onload = function(e) {
-    $('#publicacion').prepend('<div class="col s12 m12 border-post"><div id="public-header" class="col s12 m12"><br><div class="col s2 m2"><img id="photoProfile" class=" img-perfil "></div><div class="col s10 m10  usersComent"><br><span class="grey-text">Publicado a las :'+getTime()+'</span><br></div><div class="col s12 m12 divider"></div></div><div id="public-body" class="col s12 m12 "><img class="img-file img-post center-block" src="#"</div><div class="col s12 m12 "><a><i class="fa fa-thumbs-o-up icon-public" id="icon-like"></i></a><a href="#"><i class="fa fa-edit icon-public"></i></a><a><i class="fa fa-share icon-public"></i></a><p class="right grey-text" id="contador"> </p><div id="add-comment" class="col s12 m12"></div></div><div class="col s12 m12 "><input id="input-comment" placeholder="Add a comment.." type="text"></div></div>');
+    $('#publicacion').prepend('<div class="col s12 m12 border-post"><div id="public-header" class="col s12 m12"><br><div class="col s2 m2"><img class=" img-perfil photoUser"></div><div class="col s10 m10  userName"></div><br><span class="grey-text">Publicado a las :'+getTime()+'</span><br><div class="col s12 m12 divider"></div></div><div id="public-body" class="col s12 m12 "><img class="img-file img-post center-block" src="#"</div><div class="col s12 m12 "><a><i class="fa fa-thumbs-o-up icon-public" id="icon-like"></i></a><a href="#"><i class="fa fa-edit icon-public"></i></a><a><i class="fa fa-share icon-public"></i></a><p class="right grey-text" id="contador"> </p><div id="add-comment" class="col s12 m12"></div></div><div class="col s12 m12 "><input id="input-comment" placeholder="Add a comment.." type="text"></div></div>');
     $('.img-post').attr('src', e.target.result);
+
+    var $photoUser = $('.photoUser');
+    var $userName = $('.userName');
+    $photoUser.attr('src', localStorage.photoURL);
+    $userName.text(localStorage.displayName);
   };
 
   reader.readAsDataURL(this.files[0]);
-  // <img class="img-file img-post center-block" src="#">
+
+
+
+
   });
 
   // comentar las publicaciones
